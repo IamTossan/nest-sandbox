2daf589ccea4913fa76ec4be1bdd4253a429a4d2
tossan <tossanmusic@gmail.com>
2023-02-17 02:21:39 +0100

    feat: garantee id-value matching in dataloader

--- src/programs/programs.dataloaders.ts
+++ src/programs/programs.dataloaders.ts
@@ -9,8 +9,13 @@ export class ProgramNodesLoader implements NestDataLoader<string, ProgramNode> {
   constructor(private readonly programService: ProgramsService) {}
 
   generateDataLoader(): DataLoader<string, ProgramNode> {
-    return new DataLoader<string, ProgramNode>((keys) =>
-      this.programService.findNodesByIds(keys),
-    );
+    return new DataLoader<string, ProgramNode>(async (keys) => {
+      const ns = await this.programService.findNodesByIds(keys);
+      const ns_as_kv = ns.reduce((acc, cur) => {
+        acc[cur.id] = cur;
+        return acc;
+      }, {});
+      return keys.map((key) => ns_as_kv[key]);
+    });
   }
 }
